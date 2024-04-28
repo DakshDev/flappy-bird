d47a6db44d526cc962fad9b293daf11a2615f9d0
DakshDev <dakshkishore99@gmail.com>
2024-04-28 19:45:48 +0500

    pillars add random pos in last

--- assets/js/script.js
+++ assets/js/script.js
@@ -92,6 +92,13 @@ function flappyBottomControl(){
 
 // Game Over
 function gameOver(){
+    let PillarParent = document.querySelector("[pillars]");
+    PillarParent.innerHTML = `<div top></div><div bottom></div>`;
+    increasePillar();
+    increasePillar();
+    increasePillar();
+
+
     flappyObj.initTopPerceVal = board.clientHeight / 2 - flappyBird.clientHeight;
     flappyObj.initLeftPercVal = 30;
     flappyObj.flappyBeforeRotate = -20;
@@ -123,25 +130,53 @@ function isCollabToBoard(){
 
 
 
-
-
-
-
-
 // Pillars Logic
 
+let pillarIncVal = 0;
 
 function pillarFun(){
-let pillarImage = document.querySelectorAll("[pillars] div img");
-let topPillar = document.querySelector("[pillars] [top]");
-let BottomPillar = document.querySelector("[pillars] [bottom]");
+    
+
+    let pillarImage = document.querySelectorAll("[pillars] div img");
 
     for(let i=0; i<pillarImage.length; i++){
         pillarImage[i].style.right = `${flappyObj.pillarImageVal}px`;
     }
+
+    if(pillarImage[pillarIncVal].getBoundingClientRect().left < board.getBoundingClientRect().left){
+        increasePillar();
+        pillarIncVal++;
+    }
+
 }
 
+
+// Move Pillars
 function pillarsMove(){
     flappyObj.pillarImageVal+= 4;
     pillarFun();
 }
+
+
+
+
+
+// Add Pillars
+function increasePillar(){
+    let randomNum = Math.floor(Math.random() * (301)) - 150;
+
+    let topPillar = document.querySelector("[pillars] [top]");
+    let BottomPillar = document.querySelector("[pillars] [bottom]");
+
+    let topImg = '<img src="./assets/img/top-pillar.png">';
+    let btmImg = '<img src="./assets/img/bottom-pillar.png">';
+
+    topPillar.insertAdjacentHTML("beforeend",topImg)
+    BottomPillar.insertAdjacentHTML("beforeend",btmImg)
+
+    topPillar.lastElementChild.style.top = `${randomNum}px`;
+    BottomPillar.lastElementChild.style.top = `${randomNum}px`;
+}
+increasePillar();
+increasePillar();
+increasePillar();
